a13c7f95adae3526ceb99d32e4b4fa7856e7c776
Mike Vargo <mfvargo@gmail.com>
2023-01-15 18:19:31 -0800

    refactor audio and websocket threads

--- src/broadcast_server.rs
+++ src/broadcast_server.rs
@@ -1,18 +1,11 @@
 use crate::{
-    box_error::BoxError,
-    config::Config,
+    audio_thread, box_error::BoxError, broadcast_websocket, config::Config,
     jam_nation_api::JamNationApi,
-    jam_packet::JamMessage,
-    player_list::{get_micro_time, PlayerList},
 };
 use std::{
-    io::ErrorKind,
-    net::UdpSocket,
     thread::{self, sleep},
     time::Duration,
 };
-use tungstenite::{connect, Message};
-use url::Url;
 
 pub fn run(git_hash: &str) -> Result<(), BoxError> {
     // This is the entry point for the broadcast server
@@ -29,39 +22,39 @@ pub fn run(git_hash: &str) -> Result<(), BoxError> {
     let room_port = port.clone();
 
     // Create an api endpoint and register this server
+    // TODO: figure out way to get lan ip and mac address
     let mut api = JamNationApi::new(api_url.as_str(), "10.10.10.10", "test:mac", git_hash);
     while !api.has_token() {
         let _register = api.broadcast_unit_register();
         // Activate the room
         let _room_activate = api.activate_room(port)?;
         if !api.has_token() {
+            // can't connect to rtjam-nation.  sleep and then keep trying
             sleep(Duration::new(2, 0));
         }
     }
 
-    // Now we have the token, we can pass it to the websocket thread
+    // Now we have the token, we can pass it to the websocket thread along with the websocket url
     let token = String::from(api.get_token());
     let _websocket_handle = thread::spawn(move || {
-        let _res = websocket_thread(&token, &ws_url);
+        let _res = broadcast_websocket::websocket_thread(&token, &ws_url);
     });
 
     // Create a thread to host the room
     let _room_handle = thread::spawn(move || {
-        let _res = audio_thread(room_port);
+        let _res = audio_thread::run(room_port);
     });
 
-    // Now the main thread will run the broadcast keepalive code
+    // Now this main thread will run the broadcast keepalive code
     loop {
         while api.has_token() == true {
             // While in this loop, we are going to ping every 10 seconds
             let ping = api.broadcast_unit_ping()?;
-            // println!("ping: {}", ping.pretty(2));
             if ping["broadcastUnit"].is_null() {
                 // Error in the ping.  better re-register
                 api.forget_token();
             } else {
                 // Successful ping.. Sleep for 10
-                // println!("ping!");
                 sleep(Duration::new(10, 0));
             }
         }
@@ -70,7 +63,6 @@ pub fn run(git_hash: &str) -> Result<(), BoxError> {
             let _register = api.broadcast_unit_register();
             // Activate the room
             let _room_activate = api.activate_room(port)?;
-            // println!("roomActivate: {}", _room_activate.pretty(2));
         }
         // This is the timer between registration attempts
         sleep(Duration::new(2, 0));
@@ -80,91 +72,4 @@ pub fn run(git_hash: &str) -> Result<(), BoxError> {
     // let _res = room_handle.join();
     // let _res = websocket_handle.join();
     // Ok(())
-}
-
-fn websocket_thread(_token: &str, ws_url: &str) -> Result<(), BoxError> {
-    loop {
-        let con_result = connect(Url::parse(ws_url).unwrap());
-        match con_result {
-            Ok(res) => {
-                // connect attempt was tried
-                let (mut sock, resp) = res;
-                dbg!(resp);
-                let mut connected = true;
-                while connected {
-                    let res_msg = sock.read_message();
-                    match res_msg {
-                        Ok(msg) => {
-                            // We got a message from the websocket
-                            if msg.is_text() {
-                                handle_websocket_message(&msg);
-                            }
-                        }
-                        Err(e) => {
-                            dbg!(e);
-                            connected = false;
-                        }
-                    }
-                }
-            }
-            Err(e) => {
-                dbg!(e);
-            }
-        }
-
-        // pause before trying to connect again
-        sleep(Duration::new(2, 0));
-    }
-}
-
-fn handle_websocket_message(msg: &Message) -> () {
-    dbg!(msg);
-}
-
-fn audio_thread(port: u32) -> Result<(), BoxError> {
-    // So let's create a UDP socket and listen for shit
-    let sock = UdpSocket::bind(format!("0.0.0.0:{}", port))?;
-    sock.set_read_timeout(Some(Duration::new(1, 0)))?;
-    let mut players = PlayerList::build();
-    let mut msg = JamMessage::build();
-    let mut cnt: u64 = 0;
-
-    loop {
-        cnt += 1;
-        let res = sock.recv_from(msg.get_buffer());
-        // get a timestamp to use
-        let now_time = get_micro_time();
-        // update the player list
-        players.prune(now_time);
-        match res {
-            Ok(r) => {
-                let (amt, src) = r;
-                if cnt % 1000 == 0 {
-                    println!("got {} bytes from {}", amt, src);
-                    println!("player: {}", players);
-                }
-                // check if the packet was good
-                if amt <= 0 || !msg.is_valid(amt) || !players.is_allowed(msg.get_client_id()) {
-                    continue;
-                }
-                // set the server timestamp
-                msg.set_server_time(now_time.try_into()?);
-                // Update this player with the current time
-                players.update_player(now_time, msg.get_client_id(), src);
-                for player in players.get_players() {
-                    if player.address != src {
-                        // don't send echo back
-                        // send the packet
-                        sock.send_to(&msg.get_buffer()[0..amt], player.address)?;
-                    }
-                }
-            }
-            Err(e) => match e.kind() {
-                ErrorKind::WouldBlock => {}
-                other_error => {
-                    panic!("my socket went nuts! {}", other_error);
-                }
-            },
-        }
-    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 extern crate json;
 
+pub mod audio_thread;
 pub mod box_error;
 pub mod broadcast_server;
+pub mod broadcast_websocket;
 pub mod config;
 pub mod jam_nation_api;
 pub mod jam_packet;

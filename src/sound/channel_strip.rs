45d932a1435a94ebc716a150b91019ecd6e89973
Mike Vargo <mfvargo@gmail.com>
2023-02-03 14:58:47 -0800

    add new power meter to ChannelStrip

--- src/sound/channel_strip.rs
+++ src/sound/channel_strip.rs
@@ -1,4 +1,4 @@
-use crate::utils::clip_float;
+use crate::{dsp::power_meter::PowerMeter, utils::clip_float};
 use std::fmt;
 
 use super::{fader::Fader, jitter_buffer::JitterBuffer};
@@ -7,6 +7,7 @@ pub struct ChannelStrip {
     fader: Fader,
     gain: f32,
     buffer: JitterBuffer,
+    level: PowerMeter,
 }
 
 impl ChannelStrip {
@@ -15,6 +16,7 @@ impl ChannelStrip {
             fader: Fader::new(),
             gain: 1.0,
             buffer: JitterBuffer::build(),
+            level: PowerMeter::new(),
         }
     }
     pub fn calc_values(&self, in_val: f32) -> (f32, f32) {
@@ -32,6 +34,7 @@ impl ChannelStrip {
     pub fn mix_into(&mut self, out_a: &mut [f32], out_b: &mut [f32]) -> () {
         // First get some data from the buff
         let samps = self.buffer.get(out_a.len());
+        self.level.add_frame(&samps, self.gain);
         let mut i: usize = 0;
         for v in samps {
             let (l, r) = self.calc_values(v);
@@ -39,6 +42,15 @@ impl ChannelStrip {
             out_b[i] = out_b[i] + r;
             i += 1;
         }
+    }
+    pub fn get_power_avg(&self) -> f64 {
+        self.level.get_avg()
+    }
+    pub fn get_power_peak(&self) -> f64 {
+        self.level.get_peak()
+    }
+    pub fn get_depth(&self) -> f64 {
+        self.buffer.avg_depth()
     }
     pub fn add_data(&mut self, audio: &[f32]) -> () {
         self.buffer.append(audio);
@@ -50,8 +62,12 @@ impl fmt::Display for ChannelStrip {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(
             f,
-            "[ gain: {}, fade: {}, buffer: {} ]",
-            self.gain, self.fader, self.buffer
+            "[ gain: {:.2}, fade: {:.2}, peak: {:.2}, avg: {:.2}, \n\tbuffer: {} ]",
+            self.gain,
+            self.fader,
+            self.level.get_peak(),
+            self.level.get_avg(),
+            self.buffer
         )?;
         write!(f, " ]\n")
     }

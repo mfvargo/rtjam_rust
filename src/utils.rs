173d1b350ebcad761695e7b8fb5e8c5de58f8e27
Mike Vargo <mfvargo@gmail.com>
2023-02-03 15:00:16 -0800

    add functions to calculate frame power in db

--- src/utils.rs
+++ src/utils.rs
@@ -24,6 +24,25 @@ pub fn clip_float(v: f32) -> f32 {
     v
 }
 
+pub fn get_frame_power_in_db(frame: &[f32]) -> f32 {
+    // linear calcution.  sum of the squares / number of values
+    if frame.len() == 0 {
+        return to_db(0.0);
+    }
+    let mut pow: f32 = 0.0;
+    for v in frame {
+        pow = pow + f32::powi(*v, 2);
+    }
+    to_db(pow / frame.len() as f32)
+}
+
+pub fn to_db(v: f32) -> f32 {
+    if v > 0.000_000_1 {
+        return 10.0 * f32::log10(v);
+    }
+    -60.0
+}
+
 #[cfg(test)]
 
 mod test_utils {
@@ -44,5 +63,12 @@ mod test_utils {
         assert_eq!(clip_float(0.1), 0.1);
         assert_eq!(clip_float(-1.3), -1.0);
         assert_eq!(clip_float(2.3), 1.0);
+    }
+    #[test]
+    fn get_frame_power() {
+        let frame = [0.0; 128];
+        assert_eq!(get_frame_power_in_db(&frame), -60.0);
+        let frame = [0.5; 128];
+        assert_eq!(get_frame_power_in_db(&frame).round(), -6.0);
     }
 }

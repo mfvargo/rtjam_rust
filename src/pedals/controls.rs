f597976b7e39b4bfde6e707fbf13339bbee961ae
Mike Vargo <mfvargo@gmail.com>
2023-02-09 15:57:10 -0800

    Recreate PedalBoards Fixes #17

--- src/pedals/controls.rs
+++ src/pedals/controls.rs
@@ -1,10 +1,9 @@
 use serde_json::{json, Value};
 
-#[derive(ToPrimitive, FromPrimitive)]
 pub enum SettingType {
-    Float = 0,
-    Int,
-    Boolean,
+    Float(f64),
+    Int(i64),
+    Boolean(bool),
 }
 
 #[derive(ToPrimitive, FromPrimitive)]
@@ -39,7 +38,7 @@ impl PedalSetting {
             max: 100.0,
             step: 1.0,
             units: SettingUnit::Linear,
-            setting_type: SettingType::Float,
+            setting_type: SettingType::Float(0.0),
         }
     }
     pub fn to_json(&self) -> Value {
@@ -52,8 +51,33 @@ impl PedalSetting {
           "max": self.max,
           "step": self.step,
           "units": num::ToPrimitive::to_usize(&self.units),
-          "type": num::ToPrimitive::to_usize(&self.setting_type),
+          "type": self.setting_type_to_json(),
         })
+    }
+    fn setting_type_to_json(&self) -> usize {
+        match self.setting_type {
+            SettingType::Float(_f) => 0,
+            SettingType::Int(_i) => 1,
+            SettingType::Boolean(_b) => 2,
+        }
+    }
+    pub fn get_value_float(&self) -> Option<f64> {
+        match self.setting_type {
+            SettingType::Float(f) => Some(f),
+            _ => None,
+        }
+    }
+    pub fn is_true(&self) -> Option<bool> {
+        match self.setting_type {
+            SettingType::Boolean(b) => Some(b),
+            _ => None,
+        }
+    }
+    pub fn get_integer_value(&self) -> Option<i64> {
+        match self.setting_type {
+            SettingType::Int(i) => Some(i),
+            _ => None,
+        }
     }
 }
 
@@ -69,10 +93,28 @@ mod test_pedal_setttings {
     }
 
     #[test]
-    fn can_json() {
+    fn can_json_out() {
         let setting = PedalSetting::build();
         let j_val = setting.to_json();
         println!("jval: {}", j_val);
         assert_eq!(j_val["name"], setting.name);
+    }
+    #[test]
+    fn can_json_in() {
+        let data = r#"
+        {
+            "index": 0,
+            "labels": [],
+            "max": 1,
+            "min": 0,
+            "name": "bypass",
+            "step": 1,
+            "type": 2,
+            "units": 4,
+            "value": false
+          }
+        "#;
+        let jval: Value = serde_json::from_str(data).unwrap();
+        println!("json: {}", jval);
     }
 }

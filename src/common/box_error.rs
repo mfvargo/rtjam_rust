32981ad15069a1c46765f0edfdaaf7db8a8effc2
Mike Vargo <mfvargo@gmail.com>
2023-03-12 18:54:34 -0700

    finish off last of common module docs

--- src/common/box_error.rs
+++ src/common/box_error.rs
@@ -1,3 +1,9 @@
+//! type created so that the errors could work multi-threaded.
+//!
+//! To be very honest, I don't really know why I needed this, but it allows
+//! me to "move" things into threads that I could not otherwise.
+//!
+//! TODO: Figure out why I need this.
 pub type BoxError = std::boxed::Box<
     dyn std::error::Error // must implement Error to satisfy ?
         + std::marker::Send // needed for threads

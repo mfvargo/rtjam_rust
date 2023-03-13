71f741ddca114c2df7696f336a59b214c733c7aa
Mike Vargo <mfvargo@gmail.com>
2023-03-12 17:40:29 -0700

    add test program for Darius

---
+++ examples/darius.rs
@@ -0,0 +1,42 @@
+use rtjam_rust::common::box_error::BoxError;
+use rtjam_rust::common::websocket::websocket_thread;
+use std::{
+    sync::mpsc,
+    thread::{self, sleep},
+    time::Duration,
+};
+
+fn main() -> Result<(), BoxError> {
+    // most simple app to test websocket
+
+    let (to_ws_tx, to_ws_rx): (
+        mpsc::Sender<serde_json::Value>,
+        mpsc::Receiver<serde_json::Value>,
+    ) = mpsc::channel();
+    let (from_ws_tx, from_ws_rx): (
+        mpsc::Sender<serde_json::Value>,
+        mpsc::Receiver<serde_json::Value>,
+    ) = mpsc::channel();
+    let _websocket_handle = thread::spawn(move || {
+        let _res = websocket_thread(
+            "darius_room",
+            "ws://rtjam-nation.basscleftech.com/primus",
+            from_ws_tx,
+            to_ws_rx,
+        );
+    });
+
+    loop {
+        let res = from_ws_rx.try_recv();
+        match res {
+            Ok(m) => {
+                println!("websocket message: {}", m);
+            }
+            Err(_e) => {
+                // dbg!(e);
+            }
+        }
+        // This is the timer between read attempts
+        sleep(Duration::new(1, 0));
+    }
+}

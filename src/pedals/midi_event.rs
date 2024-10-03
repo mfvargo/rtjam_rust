bb444cee011de918860438f59479894df43b5ec7
Mike Vargo <mfvargo@gmail.com>
2024-10-03 14:30:39 -0700

    Add wmidi library for prettier parsing

--- src/pedals/midi_event.rs
+++ src/pedals/midi_event.rs
@@ -5,24 +5,50 @@ use serde::Serialize;
 pub struct MidiEvent {
     #[serde(rename = "type")]
     e_type: usize,
-    channel: usize,
-    note: usize,
-    velocity: usize
+    channel: u8,
+    note: u8,
+    velocity: u8
 }
 
 impl MidiEvent {
     pub fn new(e: RawMidi) -> MidiEvent {
-        match e.bytes[0] {
-            192 => {
-                MidiEvent { e_type: 4, channel: 0, note: e.bytes[1] as usize, velocity: 0 }
+        let res = Self::handle_midi_message(e.bytes);
+        match res {
+            Ok(m) => {
+                println!("{:?}", m);
+                match m {
+                    wmidi::MidiMessage::ProgramChange(chan, prog) => {
+                        MidiEvent { e_type: 4, channel: chan as u8, note: prog.into(), velocity: 0 }
+                    }
+                    wmidi::MidiMessage::ControlChange(chan, note , val) => {
+                        MidiEvent { e_type: 4, channel: 0, note: note.into(), velocity: val.into() }
+                    }
+                    _ => {
+                        MidiEvent { e_type: 8, channel: 0, note: 0, velocity: 0 }
+                    }
+                }
             }
-            176 => {
-                MidiEvent { e_type: 3, channel: 0, note: e.bytes[1] as usize, velocity: e.bytes[2] as usize }
-            }
-            _ => {
-                println!("Midi: {:?}", e);
+            Err(e) => {
+                dbg!(e);
                 MidiEvent { e_type: 8, channel: 0, note: 0, velocity: 0 }
             }
         }
+        // match e.bytes[0] {
+        //     192 => {
+        //         MidiEvent { e_type: 4, channel: 0, note: e.bytes[1] as usize, velocity: 0 }
+        //     }
+        //     176 => {
+        //         MidiEvent { e_type: 3, channel: 0, note: e.bytes[1] as usize, velocity: e.bytes[2] as usize }
+        //     }
+        //     _ => {
+        //         println!("Midi: {:?}", e);
+        //         MidiEvent { e_type: 8, channel: 0, note: 0, velocity: 0 }
+        //     }
+        // }
     }
+
+    fn handle_midi_message(bytes: &[u8]) -> Result<wmidi::MidiMessage, wmidi::FromBytesError> {
+        Ok(wmidi::MidiMessage::try_from(bytes)?)
+    }
+    
 }

00d29eef31ff803ebbf7d5e8febc578d58066176
Mike Vargo <mfvargo@gmail.com>
2024-10-27 23:23:42 -0700

    put in example i2c code from rppal crate

--- src/hw_control/codec_control.rs
+++ src/hw_control/codec_control.rs
@@ -5,7 +5,14 @@ use rppal::i2c::I2c;
 use rppal::gpio::Gpio;
 use crate::common::box_error::BoxError;
 
+// Helper functions to encode and decode binary-coded decimal (BCD) values.
+fn bcd2dec(bcd: u8) -> u8 {
+    (((bcd & 0xF0) >> 4) * 10) + (bcd & 0x0F)
+}
 
+fn dec2bcd(dec: u8) -> u8 {
+    ((dec / 10) << 4) | (dec % 10)
+}
 
 
 pub struct CodecControl {
@@ -17,7 +24,6 @@ impl CodecControl {
 
         let mut codec = I2c::new()?;
         dbg!(&codec);
-        codec.set_slave_address(0x18)?;
 
         // reset the code
         let mut pin = Gpio::new()?.get(17)?.into_output();
@@ -25,6 +31,11 @@ impl CodecControl {
         // Sleep for 200msec
         sleep(Duration::new(0, 200_000_000));
         pin.set_high();
+
+        // Let the thing come up
+        sleep(Duration::new(0, 200_000_000));
+
+        codec.set_slave_address(0x18)?;
 
         // Set Reg 0 - select Page 0
         let buf: [u8; 2] = [0,0];

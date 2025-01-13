489b4f735eed9f90627b0642f8ed1773c84091f6
Mike Vargo <mfvargo@gmail.com>
2025-01-13 14:07:08 -0800

    make met time calcs absolute, no drift

--- src/server/metronome.rs
+++ src/server/metronome.rs
@@ -1,9 +1,7 @@
-use crate::common::{get_micro_time, stream_time_stat::MicroTimer};
-
 
 pub struct Metronome {
     beat: u8,
-    duration: MicroTimer,
+    duration: u128,
     tempo: u128,
 }
 
@@ -12,31 +10,24 @@ impl Metronome {
         Metronome {
             beat: 0,
             tempo: 120,
-            duration: MicroTimer::new(get_micro_time(), 1_000_000 * 60 / 120), // 120BPM in microseconds
+            duration: 1_000_000 * 60 / 120,
         }
     }
-    pub fn set_tempo(&mut self, now_time: u128,  tempo: u128) -> () {
+    pub fn set_tempo(&mut self, tempo: u128) -> () {
         if tempo > 0 {
             self.tempo = tempo;
-            self.duration = MicroTimer::new(now_time, 1_000_000 * 60 / tempo);
+            self.duration = 1_000_000 * 60 / tempo;
         }
     }
     pub fn get_tempo(&self) -> u128 {
         self.tempo
     }
     pub fn get_beat_interval(&self) -> u128 {
-        self.duration.get_interval()
+        self.duration
     }
     pub fn get_beat(&mut self, now_time: u128)  -> u8 {
-        if self.duration.expired(now_time) {
-            self.duration.advance(self.duration.get_interval());
-            self.beat += 1;
-        }
-        self.beat = self.beat % 4;
+        self.beat = (now_time / self.duration % 4) as u8;
         self.beat
-    }
-    pub fn reset_time(&mut self, now_time: u128) -> () {
-        self.duration.reset(now_time);
     }
 }
 
@@ -46,9 +37,9 @@ mod test_metronome {
 
     #[test]
     fn test_beat() {
-        let now = 1000;
+        let now = 2_000_000;
         let mut met = Metronome::new();
-        met.set_tempo(now, 120);
+        met.set_tempo(120);
         assert_eq!(met.get_beat_interval(), 1_000_000 / 2); // 120BPM = 500_000 microseconds
         assert_eq!(met.get_beat(now), 0);
         assert_eq!(met.get_beat(now + 499_999), 0);
